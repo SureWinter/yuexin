403306911d5faaef07329cb47cd44fce6c8d8027
wangshuo <surewinter@163.com>
2015-12-23 17:39:45 +0800

    initCommit

---
+++ YueXin/YueXin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YueXin
+//
+//  Created by wangshuo on 15/12/23.
+//  Copyright © 2015年 wangshuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YueXin/YueXinTests/YueXinTests.m
@@ -0,0 +1,39 @@
+//
+//  YueXinTests.m
+//  YueXinTests
+//
+//  Created by wangshuo on 15/12/23.
+//  Copyright © 2015年 wangshuo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YueXinTests : XCTestCase
+
+@end
+
+@implementation YueXinTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
